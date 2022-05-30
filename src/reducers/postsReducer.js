3aae6b7433fe211773e6d552b047cd7ce0bdbec0
Alex Ek <54472509+astroblemeal@users.noreply.github.com>
2022-05-30 10:35:17 +0200

    Update postsReducer.js

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -2,18 +2,10 @@ import { FETCH_POSTS, DELETE_POST, NEW_POST } from "../actions";
 
 const initState = { allPosts: [] };
 
-// replaced by find by id === params id in PostsShow
-// , onePost: {}
-
-// eslint-disable-next-line
 export default function (state = initState, action) {
   switch (action.type) {
     case FETCH_POSTS:
       return { ...state, allPosts: action.payload };
-
-    // replaced by find by id === params id in PostsShow
-    // case FETCH_POST:
-    //   return { ...state, onePost: action.payload };
 
     case NEW_POST:
       return { ...state, allPosts: [...state.allPosts, action.payload] };
